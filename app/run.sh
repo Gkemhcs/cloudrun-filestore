174eb16c49561ba803ea054ee238a6da026725de
Gkemhcs <gudikotieswarmani@gmail.com>
2023-07-12 07:22:47 +0000

    added

--- app/run.sh
+++ app/run.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 mkdir -p $MNT_DIR
 
 echo "Mounting Cloud Filestore."
-mount -o nolock 10.43.1.2:/gkemhcs $MNT_DIR
+mount -o nolock $NFS_IP:/vol1 $MNT_DIR
 echo "Mounting completed."
 
 node server.js
---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+echo "ENTER YOUR PROJECT ID:-"
+read PROJECT_ID
+gcloud config set project $PROJECT_ID
+gcloud services enable cloudbuild.googleapis.com compute.googleapis.com file.googleapis.com \
+vpcaccess.googleapis.com  run.googleapis.com artifactregistry.googleapis.com 
+gcloud filestore instances create nfs-server --zone=us-central1-c  \
+--tier=BASIC_HDD --file-share=name="vol1",capacity=1TB \
+--network=name="default"
+gcloud compute networks vpc-access connectors create connector-us \
+--region us-central1 \
+--subnet  10.8.0.0/28 \
+--network default \
+--min-instances 2 \
+--max-instances 3 \
+cd app
+FILESTORE_INSTANCE_IP=$(gcloud filestore instances describe nfs-server --location=ZONE --format="value(networks.ipAddresses[0])")
+gcloud run deploy frontend --region us-central1 \
+--source . \
+--allow-unauthenticated \
+--set-env-vars=NFS_IP=$FILESTORE_INSTANCE_IP \
+--vpc-connector connector-us
